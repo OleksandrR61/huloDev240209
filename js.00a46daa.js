29770cc9875b11be453c10e0b6b30636221ca9a1
OleksandrR61 <106624948+OleksandrR61@users.noreply.github.com>
2024-02-09 09:39:09 +0200

    Updates

--- js.00a46daa.js
+++ js.00a46daa.js
@@ -554,7 +554,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53299" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57051" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
